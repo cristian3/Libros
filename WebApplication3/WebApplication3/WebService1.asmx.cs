36b623aaa10ab7af188e52274f48db2f4ceb1700
Cristian <cristiannavia3@hotmail.com>
2014-11-25 15:00:09 +0100

    asas

--- WebApplication3/WebApplication3/WebService1.asmx.cs
+++ WebApplication3/WebApplication3/WebService1.asmx.cs
@@ -25,20 +25,21 @@ namespace WebApplication3
         // [System.Web.Script.Services.ScriptService]
         
             [WebMethod]
-            public int NuevoLibro(string nombre, String categoria, String descripcion)
+            public int NuevoLibro(string nombre, String categoria, String descripcion,String URL)
             {
                SqlConnection con =
                     new SqlConnection(@"Data Source=1fbb1ef7-3ab6-440e-b714-a3ee0010dc18.sqlserver.sequelizer.com;Initial Catalog=db1fbb1ef73ab6440eb714a3ee0010dc18;User ID=pdbzjhhhipccekmj;Password=EY7V2wNGCFLfWgpZhypvWHZAsQuf4FJ8GFoHjUmnBDyXMkDnvm6ShhH67DBkbGM6;Integrated Security=False");
 
                 con.Open();
 
-                string sql = "INSERT INTO LIBRO (Nombre,Categoria,Descripcion) VALUES (@nombre, @categoria,@descripcion)";
+                string sql = "INSERT INTO LIBRO (Nombre,Categoria,Descripcion,URL) VALUES (@nombre, @categoria,@descripcion,@URL)";
 
                 SqlCommand cmd = new SqlCommand(sql, con);
               
                 cmd.Parameters.Add("@nombre", System.Data.SqlDbType.NVarChar).Value = nombre;
                 cmd.Parameters.Add("@categoria", System.Data.SqlDbType.NVarChar).Value = categoria;
                 cmd.Parameters.Add("@descripcion", System.Data.SqlDbType.NVarChar).Value = descripcion;
+                cmd.Parameters.Add("@URL", System.Data.SqlDbType.NVarChar).Value = URL;
               
                 int res = cmd.ExecuteNonQuery();
 
